964a26b1bc6fdc4b95d516f120c9f953d85a0bb5
yoken.chou@nsw.co.jp <QAZwsx12>
2025-06-05 16:38:45 +0900

    ローカル環境設定をまとめ

--- src/main/java/com/example/login_demo/LoginDemoApplication.java
+++ src/main/java/com/example/login_demo/LoginDemoApplication.java
@@ -4,6 +4,10 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @SpringBootApplication
 public class LoginDemoApplication {
@@ -15,5 +19,17 @@ public class LoginDemoApplication {
 	@Bean
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
+	}
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.csrf(csrf -> csrf.disable()) // 如果你不需要CSRF保护
+				.authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll());
+		return http.build();
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 }
